bf988cf89fa1794bc18087bba754345e3ba1ff8d
InsuMoon2 <soo93689314@gmail.com>
2025-11-17 21:48:28 +0900

    chore : ImGui 테스트

--- Client/Code/CPlayer.cpp
+++ Client/Code/CPlayer.cpp
@@ -229,15 +229,37 @@ void CPlayer::Key_Input(const _float& timeDelta)
 
 void CPlayer::TempImGuiRender()
 {
-    ImGui::Begin("Player State");
+    if (ImGui::Begin("Player Inspector"))
+    {
+        // TransformComponent
+        if (m_TransformCom && ImGui::CollapsingHeader("Transform Component", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            const _vec3& pos = m_TransformCom->Get_Pos();
 
-    ImGui::Text("State : %s", Engine::ToString(m_StateCom->Get_State()));
-    ImGui::Text("Dir : %s", Engine::ToString(m_StateCom->Get_Dir()));
+            ImGui::Text("Position");
 
-    ImGui::Text("Player Pos : X: %f, Y: %f, Z: %f",
-        m_TransformCom->Get_Pos().x,
-        m_TransformCom->Get_Pos().y,
-        m_TransformCom->Get_Pos().z);
+            ImGui::Text("X :");
+            ImGui::SameLine();
+            ImGui::InputFloat("##PlayerPosX", (float*)&pos.x);
+
+            ImGui::Text("Y :");
+            ImGui::SameLine();
+            ImGui::InputFloat("##PlayerPosY", (float*)&pos.y);
+
+            ImGui::Text("Z :");
+            ImGui::SameLine();
+            ImGui::InputFloat("##PlayerPosZ", (float*)&pos.z);
+
+            m_TransformCom->Set_Pos(pos);
+        }
+
+        // StateComponent
+        if (m_StateCom && ImGui::CollapsingHeader("State Component", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            ImGui::Text("State : %s", Engine::ToString(m_StateCom->Get_State()));
+            ImGui::Text("Dir   : %s", Engine::ToString(m_StateCom->Get_Dir()));
+        }
+    }
 
     ImGui::End();
 }
